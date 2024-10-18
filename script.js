0c0c70828406317272188b9507c10d793325e768
fatih-monyong <muhammad.fatih@student.alhazen.academy>
2024-10-18 14:10:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = "ahmad";
+console.log(name);
+name = "budi";
+console.log(name);
+name = "asep";
+console.log(name);
+
+let umur = 20;
+console.log(umur);
+umur = 31;
+console.log(umur);
+
+const alamat = "bogor";
+console.log(alamat);
+
+let number1 = prompt("masukan angka pertama :");
+let number2 = prompt("masukan angka kedua :");
+let result = parseInt(number1) + parseInt(number2);
+alert(result)
+
+fruit=["banana","apple","orange","durian"]
+alert(fruit[3])
